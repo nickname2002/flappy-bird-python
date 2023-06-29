6e7b4e8cb5c0169fab9fb1215516d6c2fa2e5400
Nick Jordan <nick@jorcademy.nl>
2023-06-29 11:30:06 +0200

    [FEATURE] Implement player movement.

--- jorcademy_python_template-main/game.py
+++ jorcademy_python_template-main/game.py
@@ -8,7 +8,7 @@ SCREEN_HEIGHT = 512
 
 # === GAME OBJECTS === #
 # Bird
-bird: Bird = GameObject(400, 200, 34, 24, "sprites/bluebird-upflap.png")
+bird: Bird = Bird(400, 200, 34, 24, "sprites/bluebird-upflap.png")
 
 # Backdrops
 backdrops: list = [Backdrop(144, 256, 288, 512, "sprites/background-night.png"),
@@ -28,6 +28,63 @@ pipes: list = [Pipe(600, 360, 52, 320, "sprites/pipe-green.png"),
                Pipe(0, -90, 52, 320, "sprites/pipe-green-rotated.png")]
 
 
+# Make pipe reset position when out of frame
+def reset_pipe_positions():
+    space_between_pipes: int = 440
+
+    for i in range(3):
+       # Fetch pair of pipes
+       pipe1 = pipes[i * 2]
+       pipe2 = pipes[i * 2 + 1]
+
+       # Update pipe positions when out of frame
+       if pipe1.out_of_frame():
+           # Generate random y position
+           pipe1.reset_pipe_position()
+           pipe2.y = pipe1.y - space_between_pipes
+
+           # Place pipes at the right side of frame
+           pipe1.place_right_of_frame()
+           pipe2.place_right_of_frame()
+
+
+# Handle user input
+def user_input():
+    if key_space_down:
+        bird.flap()
+    else:
+        bird.fall()
+
+
+# Update objects
+def update_game_objects():
+    # Update pipes
+    for bd in backdrops:
+        bd.update()
+    
+    # Update pipes
+    for pipe in pipes:
+        pipe.update()
+    reset_pipe_positions()
+
+    # Update bird
+    bird.update()
+    
+
+# Draw objects
+def draw_game_objects():
+    # Draw backdrops
+    for bd in backdrops:
+        bd.draw()
+
+    # Draw pipes
+    for pipe in pipes:
+        pipe.draw()
+
+    # Draw bird
+    bird.draw()
+
+
 # Game setup
 def setup() -> None:
     screen(SCREEN_WIDTH, SCREEN_HEIGHT)
@@ -37,21 +94,7 @@ def setup() -> None:
 
 # Game update
 def draw() -> None:
-    # Update objects
-    for bd in backdrops:
-        bd.update()
-    
-    for pipe in pipes:
-        pipe.update()
+    user_input()
+    update_game_objects()
+    draw_game_objects()
 
-    # Draw objects
-    for bd in backdrops:
-        bd.draw()
-
-    for pipe in pipes:
-        pipe.draw()
-
-    bird.draw()
-    
-# TODO: Pipe pairing and proper movement
-# TODO: Wrapping draws and updates inside separate functions for clarity
--- jorcademy_python_template-main/game_object.py
+++ jorcademy_python_template-main/game_object.py
@@ -1,4 +1,11 @@
 from jorcademy import *
+import random
+
+# CONSTANTS
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 512
+GRAVITY = 0.981
+
 
 class GameObject:
     def __init__(self, x, y, width, height, pathname):
@@ -13,8 +20,14 @@ class GameObject:
         self.x -= self.speed
 
         # Reset position when out of screen
-        if self.x < -self.width / 2: 
-            self.x = 800 + self.width / 2
+        if self.out_of_frame(): 
+            self.place_right_of_frame()
+            
+    def place_right_of_frame(self):
+        self.x = 800 + self.width / 2
+    
+    def out_of_frame(self):
+        return self.x + 1/2 * self.width < 0
 
     def update(self):
         pass 
@@ -25,16 +38,45 @@ class GameObject:
 
 class Bird(GameObject):
     def __init__(self, x, y, width, height, pathname):
-        super().__init__(x, y, width, pathname)
+        self.velocity_y = 0.5
+        self.speed = 1
+        self.min_velocity_y = -3
+        self.max_velocity_y = 4
+        super().__init__(x, y, width, height, pathname)
 
-    def update():
-        pass
+    # Change position based on velocity
+    def move(self):
+        self.y += self.velocity_y
+
+    def ground_hit(self):
+        return (self.y + self.height / 2) >= SCREEN_HEIGHT
+
+    def fall(self):
+        if self.velocity_y < self.max_velocity_y:
+            self.velocity_y += GRAVITY * self.speed
+
+    def flap(self):
+        if self.velocity_y > self.min_velocity_y:
+            self.velocity_y -= 2 * self.speed
+
+    def update(self):
+        print(self.ground_hit())
+        if not self.ground_hit():
+            self.move()
 
 
 class Pipe(GameObject):
     def __init__(self, x, y, width, height, pathname):
         super().__init__(x, y, width, height, pathname)
-        self.speed = 1.5
+        self.speed = 3.5
+
+    def reset_pipe_position(self):
+        min_y = 390
+        max_y = 600
+        self.y = random.randrange(int(min_y), int(max_y))
+
+    def move(self):
+        self.x -= self.speed
 
     def update(self):
         self.move()
